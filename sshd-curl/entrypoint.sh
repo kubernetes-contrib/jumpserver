9a5c5ac7e7cc614092f012142295ceacd783f879
Max Cai <zhiyuan.cai@gmail.com>
2019-02-12 14:09:54 +0900

    change folder name

--- sshd-curl/entrypoint.sh
+++ sshd-curl/entrypoint.sh
@@ -4,4 +4,9 @@ ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
 ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
 ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
 
+if [ -f "$KEY_FILE" ] then
+    cp ${KEY_FILE} /root/.ssh/authorized_keys
+fi
+chmod 600 /root/.ssh/authorized_keys
+
 /usr/sbin/sshd -D
